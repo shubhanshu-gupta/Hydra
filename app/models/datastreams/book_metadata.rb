934de9a25bcb7112f7f352f29d066f8d116d5bd7
Shubhanshu Gupta <shubhanshu2007@gmail.com>
2015-01-29 19:05:14 +0530

    OM terminology for book model defined after installing active-fedora gem

--- app/models/datastreams/book_metadata.rb
+++ app/models/datastreams/book_metadata.rb
@@ -2,6 +2,23 @@
 #  `rails generate active_fedora::model Book`
 class BookMetadata < ActiveFedora::OmDatastream
 
+set_terminology do |t|
+    t.root(path: "fields")
+    t.title
+    t.author
+  end
+
+  def self.xml_template
+    Nokogiri::XML.parse("<fields/>")
+  end
+
+  def prefix
+    # set a datastream prefix if you need to namespace terms that might occur in multiple data streams 
+    ""
+  end
+
+end
+
   # Define a terminology for parsing this XML document
   # See: https://github.com/projecthydra/om/wiki/Tame-your-XML-with-OM
   #
